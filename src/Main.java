3594bee0fa1b4dfd88ae3387683f3e5fcfc54fec
Jan <wittmannjan1984@gmail.com>
2022-11-06 14:28:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        long Sekunden = 3011456567L;
+        long Minut = Sekunden / 60;
+        long Stunden = Minut / 60;
+        long Tage = Stunden / 24;
+        long Jahren = Tage / 365;
+
+
+
+
+
+
+        System.out.println(Jahren);
+    }
+}
